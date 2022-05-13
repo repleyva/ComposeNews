2607556b3c68599d85dc9adad5f95cbf2d5225ce
repleyva <rusvelpassos@gmail.com>
2022-05-13 08:58:50 -0500

    fix

--- NewsCompose/app/src/main/java/repleyva/dev/newscompose/repository/NewsRepositoryImpl.kt
+++ NewsCompose/app/src/main/java/repleyva/dev/newscompose/repository/NewsRepositoryImpl.kt
@@ -5,11 +5,11 @@ import repleyva.dev.newscompose.model.NewsEntity
 class NewsRepositoryImpl: NewsRepository {
 
     override suspend fun getTopHeadLines(country: String): List<NewsEntity> {
-        TODO("Not yet implemented")
+        TODO("Not yet")
     }
 
     override fun getNew(title: String): NewsEntity {
-        TODO("Not yet implemented")
+        TODO("Not yet")
     }
 
 }
